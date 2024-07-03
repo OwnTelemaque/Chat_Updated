31584bed97697f65cf3f552b950edfdc2d551b31
OwnTelemaque <nico.degouve@gmail.com>
2024-07-02 17:29:03 -1000

    chamgement chemins

--- public/javascripts/client_chat.js
+++ public/javascripts/client_chat.js
@@ -1,5 +1,5 @@
 //Connexion a socket.io
-var socket = io.connect('http://localhost:3000');     //Si on bosse en local
+var socket = io.connect('https://owntelemaque-chat.koyeb.app:3000');     //Si on bosse en local
 
 //Liste des amis de l'utilisateur
 var liste_amis = [];
